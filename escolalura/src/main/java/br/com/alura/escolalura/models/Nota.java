64c60c7b56defb5df9362045aea6cc03e79cecb5
juniorhc <junior.jaava@gmail.com>
2018-02-27 21:57:38 -0300

    Cadastrar page Cadastrar controler Model Aluno and his attributes

---
+++ escolalura/src/main/java/br/com/alura/escolalura/models/Nota.java
@@ -0,0 +1,17 @@
+package br.com.alura.escolalura.models;
+
+public class Nota {
+
+	private Double valor;
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+}
