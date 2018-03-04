9185cdeed1ea9915941be9075dd233a5db07c8a9
juniorhc <junior.jaava@gmail.com>
2018-03-04 17:33:07 -0300

    Listar page done: - html page - decode - dao

--- escolalura/src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
+++ escolalura/src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
@@ -1,5 +1,7 @@
 package br.com.alura.escolalura.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,4 +31,12 @@ public class AlunoController {
   		return "redirect:/"; 
 	}
 
+	
+	@GetMapping("/aluno/listar")
+	public String listar(Model model){
+		
+		List<Aluno> alunos = repository.obterTodosAlunos();
+		model.addAttribute("alunos", alunos); 
+		return "aluno/listar"; 
+	}
 }
--- escolalura/src/main/java/br/com/alura/escolalura/repositorys/AlunoRepository.java
+++ escolalura/src/main/java/br/com/alura/escolalura/repositorys/AlunoRepository.java
@@ -1,10 +1,12 @@
 package br.com.alura.escolalura.repositorys;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.bson.codecs.Codec;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.types.Code;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.escolalura.codecs.AlunoCodec;
@@ -13,24 +15,49 @@ import br.com.alura.escolalura.models.Aluno;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 @Repository
 public class AlunoRepository {
+	
+	private MongoClient cliente;
+	MongoDatabase bancoDeDados;
 
-	public void salvar(Aluno aluno) {
-		
+	private MongoDatabase criarConexao() {
 		Codec<Document> codec = MongoClient.getDefaultCodecRegistry().get(Document.class);
 		AlunoCodec alunoCodec = new AlunoCodec(codec);
 		CodecRegistry registro = CodecRegistries.fromRegistries(MongoClient.getDefaultCodecRegistry(),	CodecRegistries.fromCodecs(alunoCodec));
 		MongoClientOptions options = MongoClientOptions.builder().codecRegistry(registro).build();
+		
+		cliente = new MongoClient("localhost:27017", options);
+		bancoDeDados = cliente.getDatabase("test");
+		return bancoDeDados;
+	}
 
-		MongoClient cliente = new MongoClient("localhost:27017", options);
-		MongoDatabase bancoDeDados = cliente.getDatabase("test");
-		MongoCollection<Aluno> alunos = bancoDeDados.getCollection("alunos",
-				Aluno.class);
+	public void salvar(Aluno aluno) {
+		
+		criarConexao();
+		MongoCollection<Aluno> alunos = this.bancoDeDados.getCollection("alunos", Aluno.class);
 		alunos.insertOne(aluno);
 
+	}
+	
+	public List<Aluno> obterTodosAlunos() {
+		
+		criarConexao();
+		MongoCollection<Aluno> alunos = bancoDeDados.getCollection("alunos", Aluno.class);
+		MongoCursor<Aluno> resultado = alunos.find().iterator();
+
+		List<Aluno> alunosEncontrados = new ArrayList<>();
+		while (resultado.hasNext()) {
+			Aluno aluno = resultado.next();
+			alunosEncontrados.add(aluno);
+		}
+		
+		cliente.close();
+
+		return alunosEncontrados;		
 	}
 
 }
